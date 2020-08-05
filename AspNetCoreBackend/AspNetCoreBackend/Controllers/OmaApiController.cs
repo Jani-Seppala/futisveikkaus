5342081253be902348367ddf9b2620cdeb4d80d9
Jani-Seppala <jani.seppala6@gmail.com>
2020-08-05 14:56:46 +0300

    lisätty luku ja merkkijonon palautus

---
+++ AspNetCoreBackend/AspNetCoreBackend/Controllers/OmaApiController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreBackend.Controllers
+{
+    [Route("api/oma")]
+    [ApiController]
+    public class OmaApiController : ControllerBase
+    {
+        [Route("luku")]
+        public int Luku()
+        {
+            return 123;
+        }
+
+        [Route("merkkijono")]
+        public string Merkkijono()
+        {
+            return "tietokone";
+        }
+
+    }   
+}  
